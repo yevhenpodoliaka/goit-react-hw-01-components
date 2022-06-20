ab803da44246c070488bb971792b51182f82f72d
Yevhen Podoliaka <zanp85740@gmail.com>
2022-06-20 13:27:41 +0300

    add start file task2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,9 @@
 import user from './data/user.json';
+import statistics from './data/data.json';
+import friends from './data/friends.json';
+import transactions from './data/transactions.json';
 import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
 
 export const App = () => {
   return (
@@ -22,6 +26,8 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+     <Statistics title="Upload stats" stats={statistics} />
+<Statistics stats={statistics} />
     </div>
   );
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,13 @@
+import PropTypes from 'prop-types';
+import './Statistics.module.css'
+
+function Statistics({title ,stats }) {
+
+        
+
+}
+Statistics.prototypes = {
+    
+}
+
+export default Statistics
